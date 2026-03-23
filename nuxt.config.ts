086a38f910f076ff3282d8794bf8f432ccf029a6
Igor Zabotin <igrzbtn@gmail.com>
2026-03-23 23:14:47 +0500

    page #1 done

---
+++ nuxt.config.ts
@@ -0,0 +1,63 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    'nuxt-og-image'
+  ],
+
+  ssr: true,
+
+  devtools: {
+    enabled: false
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          langs: ['php', 'blade']
+        },
+
+        toc: {
+          searchDepth: 1
+        }
+      }
+    }
+  },
+
+  experimental: {
+    asyncContext: true
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    preset: 'static',
+
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true,
+      autoSubfolderIndex: false
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  icon: {
+    provider: 'iconify'
+  }
+})
