d8e285a8224a73eaadf5b29eb33614577a192897
Igor Zabotin <igrzbtn@gmail.com>
2026-03-25 09:16:35 +0500

    builded

---
+++ _nuxt/CLCYvTbn.js
@@ -0,0 +1,1 @@
+import{r as s}from"./BHPVo4vQ.js";import t from"./C1d7DvEN.js";import{W as a,a as e,l as p,q as c,o as n}from"./D4nk9eCs.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=a();return(r,f)=>(n(),e(t,{color:"success",icon:c(o).ui.icons.tip},{default:p(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/COa1HSAg.js
@@ -0,0 +1,1 @@
+import{r as a}from"./BHPVo4vQ.js";import t from"./C1d7DvEN.js";import{W as e,a as n,l as s,q as c,o as i}from"./D4nk9eCs.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(i(),n(t,{color:"error",icon:c(o).ui.icons.caution},{default:s(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
